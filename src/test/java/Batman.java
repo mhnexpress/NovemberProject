5e3f8d7562f0b58c881fa541a1484b6f35d98763
murodhamroev <mhnexpress@gmaol.com>
2020-11-11 22:44:50 -0500

    Batman has Bat bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -4,5 +4,6 @@ public class Batman {
         System.out.println("I will catch you Joker");
         //Yes he will catch him
         //Batman has cool Batmobile
+        //Batman has cool Bat bike
     }
 }
